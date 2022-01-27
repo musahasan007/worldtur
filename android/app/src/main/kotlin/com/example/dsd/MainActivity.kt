98614013bd16e118e63adb88a8067ccafb8fc785
unknown <you@example.com>
2022-01-27 15:47:31 +0600

    new

---
+++ android/app/src/main/kotlin/com/example/dsd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dsd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+//import 'package:dsd/scr/screens/welcome.dart';
+import 'package:flutter/material.dart';
+import 'package:trevel_app/scr/screens/welcome.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Welcome(),
+));
